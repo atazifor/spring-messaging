58a7c7cc5f40cd3fd4dac05b89400114bb2e7ae2
Amin Tazifor <amin.tazifor@gmail.com>
2024-10-14 11:56:00 +0100

    use service names to connect to kafka and rabbitmq instead of using localhost or 127.0.0.1

--- src/main/java/taz/amin/springcloudstreamtutorial/service/MessageConsumer.java
+++ src/main/java/taz/amin/springcloudstreamtutorial/service/MessageConsumer.java
@@ -1,12 +1,16 @@
 package taz.amin.springcloudstreamtutorial.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.messaging.support.ErrorMessage;
 import org.springframework.stereotype.Service;
 
 import java.util.function.Consumer;
 
 @Service
 public class MessageConsumer {
+    private static Logger LOG = LoggerFactory.getLogger(MessageConsumer.class.getName());
     // Consumer function for customer-related messages
     @Bean
     public Consumer<String> invoiceInput() {
@@ -20,6 +24,13 @@ public class MessageConsumer {
     public Consumer<String> orderInput() {
         return message -> {
             System.out.println("Received order message: " + message);
+        };
+    }
+
+    @Bean
+    public Consumer<ErrorMessage> errorHandler() {
+        return errorMessage -> {
+            LOG.error("Error Processing Message");
         };
     }
 }
